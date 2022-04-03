fd2ba3253924f632dee2b4743e16c9aea443ab98
fredolouis <63505296+fredolouis@users.noreply.github.com>
2022-04-03 22:03:24 +0200

    less repetition

--- index.js
+++ index.js
@@ -1,44 +1,32 @@
 const jwt = require('jsonwebtoken');
-
+const loop =(err,req,res,next,route)=>{
+    if(route.role.length || route.secure){
+        auth.verify(err,req,res,next,route.role);
+    }
+    route.middleware.forEach(middleware=>{  middleware(req,res,next)})
+    next()
+}
 const router = (_app,_router)=>{
     _router.forEach((route)=>{
         switch (route.method){
             case "GET":
                 _app.get(route.path,(err,req,res,next)=>{
-                    if(route.role.length || route.secure){
-                        auth.verify(err,req,res,next,route.role);
-                    }
-                    route.middleware.forEach(middleware=>{  middleware(req,res,next)})
-                    next()
+                    loop(err,req,res,next,route);
                 },route.controller)
                 break;
             case "POST":
                 _app.post(route.path,(err,req,res,next)=>{
-                    if (route.role.length || route.secure){
-                        auth.verify(err,req,res,next,route.role);
-                    }
-
-                    route.middleware.forEach(middleware=>{  middleware(req,res,next)})
-                    next()
+                    loop(err,req,res,next,route);
                 },route.controller)
                 break;
             case "PUT":
                 _app.put(route.path,(err,req,res,next)=>{
-                    if (route.role.length || route.secure){
-                        auth.verify(err,req,res,next,route.role);
-                    }
-
-                    route.middleware.forEach(middleware=>{  middleware(req,res,next)})
-                    next()
+                    loop(err,req,res,next,route);
                 },route.controller)
                 break;
             case "DELETE":
                 _app.delete(route.path,(err,req,res,next)=>{
-                    if (route.role.length || route.secure){
-                        auth.verify(err,req,res,next,route.role);
-                    }
-                    route.middleware.forEach(middleware=>{  middleware(req,res,next)})
-                    next()
+                    loop(err,req,res,next,route);
                 },route.controller)
                 break;
         }
